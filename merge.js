63a26e25881bf18342be95c46b5d5fe8e3eb3a7f
Seth Vargas <shvargas01@gmail.com>
2023-11-10 01:16:23 -0600

    Finished merge and mergeSort

--- merge.js
+++ merge.js
@@ -1,5 +1,44 @@
-function merge() {}
+function merge(arr1 = [], arr2 = []) {
+  let output = [];
+  let a = 0;
+  let b = 0;
 
-function mergeSort() {}
+  while (a < arr1.length && b < arr2.length) {
+    let val1 = arr1[a];
+    let val2 = arr2[b];
 
-module.exports = { merge, mergeSort};
+    if (val1 <= val2) {
+      output.push(val1);
+      a++;
+    } else {
+      output.push(val2);
+      b++;
+    }
+  }
+
+  while (a < arr1.length) {
+    output.push(arr1[a]);
+    a++;
+  }
+  while (b < arr2.length) {
+    output.push(arr2[b]);
+    b++;
+  }
+
+  return output;
+}
+
+function mergeSort(arr = []) {
+  // BASE CASE
+  if (arr.length <= 1) return arr;
+
+  // NORMAL CASE
+  const mid = Math.floor(arr.length / 2);
+
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+
+module.exports = { merge, mergeSort };
