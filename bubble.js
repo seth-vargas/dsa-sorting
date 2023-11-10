ac0995e0e3036654f3482e69123d3931c5e5d305
Seth Vargas <shvargas01@gmail.com>
2023-11-10 01:08:38 -0600

    finished bubble sort

--- bubble.js
+++ bubble.js
@@ -1,3 +1,17 @@
-function bubbleSort() {}
+function bubbleSort(arr = []) {
+  for (let a = 0; a < arr.length; a++) {
+    let swapped = false;
+    for (let b = 0; b < arr.length - a; b++) {
+      if (arr[b] > arr[b + 1]) {
+        let temp = arr[b];
+        arr[b] = arr[b + 1];
+        arr[b + 1] = temp;
+        swapped = true;
+      }
+    }
+    if (!swapped) break;
+  }
+  return arr;
+}
 
 module.exports = bubbleSort;
